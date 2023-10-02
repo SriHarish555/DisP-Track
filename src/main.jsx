b52b85afb8ff6cbe96af5f507172e58a573ef362
Sri Harish <sriharish7635@gmail.com>
2023-10-02 21:52:36 +0530

    router main shift

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom/dist/index.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom/dist/index.js";
+import MainContent from "./components/MainContent.jsx";
+import UploadForm from "./components/UploadForm.jsx";
+import RetrieveDetails from "./components/RetrieveDetails.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<MainContent />}></Route>
+      <Route path="/upload" element={<UploadForm />}></Route>
+      <Route path="/retrieve" element={<RetrieveDetails />}></Route>
+      <Route path="*" element={<MainContent />}></Route>
+    </Routes>
+
+    {/* <App /> */}
   </BrowserRouter>
 );
