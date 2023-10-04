bc682a4a7a89b17e7ef4b0d207f7e5d39a73a246
Sri Harish <sriharish7635@gmail.com>
2023-10-04 20:55:04 +0530

    spinning bubble loader

--- src/PreLoad.jsx
+++ src/PreLoad.jsx
@@ -5,7 +5,17 @@ const PreLoad = () => {
   const App = React.lazy(() => import("./App"));
   return (
     <>
-      <React.Suspense fallback="Page is Loading.....">
+      <React.Suspense
+        fallback={
+          <ReactLoading
+            type={"spinningBubbles"}
+            color={"#33F8EF"}
+            height={100}
+            width={100}
+            className="loader"
+          />
+        }
+      >
         <App />
       </React.Suspense>
     </>
