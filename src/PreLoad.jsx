76a296db0f6c7891443bf3b2cfc1cbf0e93d31a1
Sri Harish <sriharish7635@gmail.com>
2023-10-04 21:05:11 +0530

    unwanted

--- src/PreLoad.jsx
+++ src/PreLoad.jsx
@@ -3,23 +3,17 @@ import ReactLoading from "react-loading";
 
 const PreLoad = () => {
   const App = React.lazy(() => import("./App"));
-  const [loader, setLoader] = useState(true);
-  setTimeout(() => setLoader(false), 5000);
   return (
     <>
       <React.Suspense
         fallback={
-          <>
-            {loader && (
-              <ReactLoading
-                type={"spinningBubbles"}
-                color={"#33F8EF"}
-                height={100}
-                width={100}
-                className="loader"
-              />
-            )}
-          </>
+          <ReactLoading
+            type={"spinningBubbles"}
+            color={"#33F8EF"}
+            height={100}
+            width={100}
+            className="loader"
+          />
         }
       >
         <App />
