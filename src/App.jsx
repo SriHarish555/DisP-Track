84fc14ee9528405e88cffb4f92be4b2d3f975213
Sri Harish <sriharish7635@gmail.com>
2023-10-04 19:59:39 +0530

    intial page loader

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,49 @@ import RetrieveDetails from "./components/RetrieveDetails";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
+import { useState } from "react";
+import ReactLoading from "react-loading";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const stopLoad = () => {
+    setLoading(false);
+  };
+
+  setTimeout(stopLoad, 3000);
+
   return (
     <>
-      <div className="mainApp">
-        <StateProvider initialState={initialState} reducer={reducer}>
-          <ToastContainer />
-          <BrowserRouter>
-            <Navbar />
-            <Routes>
-              {console.log("Printing")}
-              <Route path="/" element={<MainContent />}></Route>
-              <Route path="/upload" element={<UploadForm />}></Route>
-              <Route path="/retrieve" element={<RetrieveDetails />}></Route>
-              <Route path="*" element={<MainContent />}></Route>
-            </Routes>
-          </BrowserRouter>
-        </StateProvider>
-        <Footer />
-      </div>
+      {loading && (
+        <>
+          <p>Please wait while we load the page</p>
+          <ReactLoading
+            type={"cubes"}
+            color={"#33F8EF"}
+            height={100}
+            width={100}
+            className="loader"
+          />
+        </>
+      )}
+      {!loading && (
+        <div className="mainApp">
+          <StateProvider initialState={initialState} reducer={reducer}>
+            <ToastContainer />
+            <BrowserRouter>
+              <Navbar />
+              <Routes>
+                {console.log("Printing")}
+                <Route path="/" element={<MainContent />}></Route>
+                <Route path="/upload" element={<UploadForm />}></Route>
+                <Route path="/retrieve" element={<RetrieveDetails />}></Route>
+                <Route path="*" element={<MainContent />}></Route>
+              </Routes>
+            </BrowserRouter>
+          </StateProvider>
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
