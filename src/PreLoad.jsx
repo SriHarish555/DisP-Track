a17e3fd90837ef8eb9c59ef9a8cd1ff91b7d937e
Sri Harish <sriharish7635@gmail.com>
2023-10-04 21:02:05 +0530

    timeout

--- src/PreLoad.jsx
+++ src/PreLoad.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactLoading from "react-loading";
 
 const PreLoad = () => {
   const App = React.lazy(() => import("./App"));
+  const [loader, setLoader] = useState(true);
+  setTimeout(() => setLoader(false), 2000);
   return (
     <>
       <React.Suspense
         fallback={
-          <ReactLoading
-            type={"spinningBubbles"}
-            color={"#33F8EF"}
-            height={100}
-            width={100}
-            className="loader"
-          />
+          <>
+            {loader && (
+              <ReactLoading
+                type={"spinningBubbles"}
+                color={"#33F8EF"}
+                height={100}
+                width={100}
+                className="loader"
+              />
+            )}
+          </>
         }
       >
         <App />
